e7dca909cc59a24825db92978e48940cec34937e
Johan Laanstra <jlaanstra1221@hotmail.com>
2012-04-25 22:49:33 +0200

    Implemented apging support on Sites.

--- ExchangeLINQ/Sites/SitesEntry.cs
+++ ExchangeLINQ/Sites/SitesEntry.cs
@@ -1,6 +1,8 @@
 ﻿using ExchangeLINQ.Common;
 using ExchangeLINQ.Common.State;
 using ExchangeLINQ.Models;
+using ExchangeLINQ.Complex;
+using System;
 
 namespace ExchangeLINQ.Sites
 {
@@ -15,5 +17,21 @@ namespace ExchangeLINQ.Sites
 			this.Url = url;
 			this.Url.QueryUrl = UrlConstants.SitesUrl;
 		}
+
+		#region Complex
+
+		public FilteredByPage<Site> Where(Func<IPageFilter, FilterPage> f)
+		{
+			FilterPage filter = f(new InterfacesImpl());
+			return new FilteredByPage<Site>(this.Url, filter);
+		}
+
+		public FilteredByFilter<Site> Where(Func<IPageFilter, FilterFilter> f)
+		{
+			FilterFilter filter = f(new InterfacesImpl());
+			return new FilteredByFilter<Site>(this.Url, filter);
+		}
+
+		#endregion
 	}
 }
